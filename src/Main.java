41ba983ebc608509a758e775345b74944e5070ad
Tales Bontempo Cunha <tabocu@gmail.com>
2017-10-30 17:44:05 -0200

    Adding time count of each method

--- src/Main.java
+++ src/Main.java
@@ -26,48 +26,68 @@ public class Main {
 		GameTree gameTreeA = new GameTree(state);
 		
 		List<Tree<State>.Node> resultPath = new LinkedList<>();
-		Tree<State>.Node end = Facade.depthFirstSearch(gameTreeA, null, gameTreeA, resultPath);
 		
-		printResult("Depth First Search", resultPath, end);
+		long startTime = System.currentTimeMillis();
+		Tree<State>.Node end = Facade.depthFirstSearch(gameTreeA, null, gameTreeA, resultPath);
+		long endTime   = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+		
+		printResult("Depth First Search", resultPath, end, totalTime);
 	}
 	
 	private static void breadthFirstSearch(State state) {
 		GameTree gameTreeA = new GameTree(state);
 		
 		List<Tree<State>.Node> resultPath = new LinkedList<>();
+
+		long startTime = System.currentTimeMillis();
 		Tree<State>.Node end = Facade.breadthFirstSearch(gameTreeA, null, gameTreeA, resultPath);
+		long endTime   = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
 		
-		printResult("Breadth First Search", resultPath, end);
+		printResult("Breadth First Search", resultPath, end, totalTime);
 	}
 	
 	private static void uniformCostSearch(State state) {
 		GameTree gameTreeA = new GameTree(state);
 		
 		List<Tree<State>.Node> resultPath = new LinkedList<>();
-		Tree<State>.Node end = Facade.uniformCostSearch(gameTreeA, null, gameTreeA, resultPath);
 		
-		printResult("Uniform Cost Search",resultPath, end);
+		long startTime = System.currentTimeMillis();
+		Tree<State>.Node end = Facade.uniformCostSearch(gameTreeA, null, gameTreeA, resultPath);
+		long endTime   = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+		
+		printResult("Uniform Cost Search",resultPath, end, totalTime);
 	}
 	
 	private static void greedyBestFirstSearch(State state) {
 		GameTree gameTreeA = new GameTree(state);
 		
 		List<Tree<State>.Node> resultPath = new LinkedList<>();
-		Tree<State>.Node end = Facade.greedyBestFirstSearch(gameTreeA, null, gameTreeA, resultPath);
 		
-		printResult("Greedy Best First Search", resultPath, end);
+		long startTime = System.currentTimeMillis();
+		Tree<State>.Node end = Facade.greedyBestFirstSearch(gameTreeA, null, gameTreeA, resultPath);
+		long endTime   = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+		
+		printResult("Greedy Best First Search", resultPath, end, totalTime);
 	}
 	
 	private static void aStarSearch(State state) {
 		GameTree gameTreeA = new GameTree(state);
 		
 		List<Tree<State>.Node> resultPath = new LinkedList<>();
-		Tree<State>.Node end = Facade.aStarSearch(gameTreeA, null, gameTreeA, resultPath);
 		
-		printResult("Star Search", resultPath, end);
+		long startTime = System.currentTimeMillis();
+		Tree<State>.Node end = Facade.aStarSearch(gameTreeA, null, gameTreeA, resultPath);
+		long endTime   = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+		
+		printResult("Star Search", resultPath, end, totalTime);
 	}
 	
-	private static void printResult(String name, List<Tree<State>.Node> resultPath, Tree<State>.Node end) {
+	private static void printResult(String name, List<Tree<State>.Node> resultPath, Tree<State>.Node end, long totalTime) {
 		//System.out.println(end.getContent());
 		
 		System.out.println("\n" + name);
@@ -80,5 +100,6 @@ public class Main {
 		}
 		
 		System.out.println("Tamanho do caminho: " + resultPath.size());
+		System.out.println("Tempo de execução: " + totalTime + " milisegundos");
 	}	
 }
